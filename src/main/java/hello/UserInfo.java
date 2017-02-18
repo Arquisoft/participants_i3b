acce0682ee1f6eb69b8be8e18bb57dc4e739315f
Paula Tuñón <paula.tualba@gmail.com>
2017-02-18 12:20:04 +0100

    Citizen and UserInfo classes combined

--- src/main/java/hello/UserInfo.java
+++ src/main/java/hello/UserInfo.java
@@ -1,10 +1,19 @@
 package hello;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Document(collection= "users")
 public class UserInfo {
@@ -13,27 +22,70 @@ public class UserInfo {
     @JsonIgnore
     private static final Logger LOG = LoggerFactory.getLogger(UserInfo.class);
 
-    @JsonIgnore
     @Id
-    private String ID;
+    @JsonIgnore
+    private String id;
 
     @JsonIgnore
-    private final String password;
+    private String password;
+    @JsonIgnore
+    private Date birthDate;
+    @JsonIgnore
+    private String address;
+    @JsonIgnore
+    private String nationality;  
+    @JsonIgnore
+    private int pollingStation;
+    
+    private String firstName;
+    private String lastName;
+    private Integer age;
+    private String NIF;
+    private String email; 
 
-    private final String firstName;
-    private final String lastName;
-    private final String email;
-    private final Integer age;
-
-
-    public UserInfo(String password, String firstName, String lastName, String email, Integer age) {
-        this.password = password;
+    private UserInfo() { }
+    
+    public UserInfo(String firstName, String lastName, String email, Date birthDate) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
-        this.age = age;
+        this.birthDate = birthDate;
+    }
+    
+    public UserInfo(String password, String firstName, String lastName, String email, Date birthDate) {
+        this(firstName, lastName, email, birthDate);
+    	this.password = password;
     }
 
+    public UserInfo(String firstName, String lastName, String email, String birthDate,
+            String address, String nationality, String ID, String NIF,
+            String pollingStation) {
+    	
+    	this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        setBirthDate(birthDate);
+        this.address = address;
+        this.nationality = nationality;
+        this.id = ID;
+        this.NIF = NIF;
+        this.pollingStation = Integer.parseInt(pollingStation);
+    }
+    
+    public UserInfo(String[] data) {
+        this(data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8]);
+    }
+    
+    private void setBirthDate(String birthDate) {
+        DateFormat format = new SimpleDateFormat("d/M/y");
+        Date date = null;
+        try {
+            date = format.parse(birthDate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        this.birthDate = date;
+    }
 
     public String getPassword() {
         return password;
@@ -52,7 +104,73 @@ public class UserInfo {
     }
 
     public Integer getAge() {
+    	setAge(getBirthDate());
         return age;
     }
+    
+    private void setAge(Date birthdate) {
+    	LocalDate date = birthdate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        this.age = Period.between(date, LocalDate.now()).getYears();
+    }
+    
+    public String getNationality() {
+        return nationality;
+    }
+    
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getId() {
+        return id;
+    }
+    
+    public void setPassword(String pw) {
+        this.password = pw;
+    }
+
+    public String getNIF() {
+        return NIF;
+    }
+
+    public int getPollingStation() {
+        return pollingStation;
+    }
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UserInfo other = (UserInfo) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "UserInfo [ID=" + id + ", password=" + password + ", birthDate=" + birthDate + ", address=" + address
+				+ ", nationality=" + nationality + ", pollingStation=" + pollingStation + ", firstName=" + firstName
+				+ ", lastName=" + lastName + ", age=" + age + ", NIF=" + NIF + ", email=" + email + "]";
+	}
 
 }
