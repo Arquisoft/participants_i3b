e0df0476064786a3699fe5d90c32da164c1ab79a
guille <guille10000@gmail.com>
2017-02-20 01:15:07 +0100

    Fixed merging error

--- src/test/java/hello/FormControllerTest.java
+++ src/test/java/hello/FormControllerTest.java
@@ -1,6 +1,5 @@
 package hello;
 
-<<<<<<< HEAD
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
@@ -11,23 +10,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.net.URL;
-import java.nio.charset.Charset;
 import java.util.Date;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-=======
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
->>>>>>> origin/master
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.TestRestTemplate;
-<<<<<<< HEAD
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -43,34 +35,15 @@ import repository.DBService;
 @SuppressWarnings("deprecation")
 @RunWith(SpringJUnit4ClassRunner.class)
 @ComponentScan("repository")
-=======
-import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.web.client.RestTemplate;
-
-import java.net.URL;
-
-import static org.hamcrest.Matchers.containsString;
-import static org.junit.Assert.assertThat;
-
-@SuppressWarnings("deprecation")
-@RunWith(SpringJUnit4ClassRunner.class)
->>>>>>> origin/master
 @SpringApplicationConfiguration(classes = Application.class)
 @WebAppConfiguration
 @IntegrationTest({"server.port=0"})
 public class FormControllerTest {
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/master
     @Value("${local.server.port}")
     private int port;
 
     private URL base;
     private RestTemplate template;
-<<<<<<< HEAD
     private MockMvc mockMvc;
 
     @Autowired
@@ -109,21 +82,6 @@ public class FormControllerTest {
                 .andExpect(model().attribute("name1", equalTo("name")))
                 .andExpect(content().string(containsString("Name:")))
                 .andExpect(content().string(containsString("Birthdate:")));
-=======
-
-    @Before
-    public void setUp() throws Exception {
-        this.base = new URL("http://localhost:" + port + "/login");
-        //noinspection deprecation
-        template = new TestRestTemplate();
-    }
-
-
-    @Test
-    public void getLogin() throws Exception {
-        ResponseEntity<String> response = template.getForEntity(base.toString(), String.class);
-        assertThat(response.getBody(), containsString("login"));
->>>>>>> origin/master
     }
 
 }
